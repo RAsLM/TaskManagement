b96c78638d1231770e92fe9cefb3c588c1a8033e
ruslan <aslanov.r.m@gmail.com>
2018-02-20 11:30:57 +0300

    First commit

---
+++ src/main/java/com/rasl/TaskmanagmentApplication.java
@@ -0,0 +1,12 @@
+package com.rasl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagmentApplication.class, args);
+	}
+}
