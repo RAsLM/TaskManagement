865356faeb36cf6c28a3f5874cb6629ae65ad4a9
ruslan <aslanov.r.m@gmail.com>
2018-09-23 17:01:51 +0300

    Remove unnecessary imports

--- src/main/java/com/rasl/controller/MainController.java
+++ src/main/java/com/rasl/controller/MainController.java
@@ -1,14 +1,13 @@
 package com.rasl.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
 
     @RequestMapping({"/", "/home"})
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 
